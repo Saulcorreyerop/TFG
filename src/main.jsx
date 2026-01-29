a3bab63886f759d650198fcbe12f7d88a58454f6
Saul Correyero <scorreyerop01@educarex.es>
2026-01-29 14:20:34 +0100

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App.jsx'
-
-// --- 1. ESTILOS DEL MAPA ---
 import 'leaflet/dist/leaflet.css'
-
-// --- 2. CAMBIO DE TEMA A CLARO (LIGHT) ---
-import 'primereact/resources/themes/lara-light-blue/theme.css' // <--- CAMBIO AQUÍ
+import 'primereact/resources/themes/lara-light-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
-
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <HelmetProvider>
+      <PrimeReactProvider>
+        <App />
+      </PrimeReactProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
